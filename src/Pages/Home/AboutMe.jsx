b92b624f71f3e09ed68bb8fbce5774e36903dd86
NormTheStorm <mrmichaelgayle@gmail.com>
2024-01-27 15:13:53 -0500

    first commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,19 @@
+export default function AboutMe(){
+    return (
+        <section id="AboutMe" className="about--section">
+            <div className="about--section--img">
+                <img src="./img/aboutme-1.png" alt="AboutMe" />
+            </div>
+            <div className="hero--section--content--box about--section--box">
+                <div className="hero--section--content">
+                    <h1 className="skills--section--heading">
+                        About Me
+                    </h1>
+                    <p className="hero--section-description">
+                  Meet an accomplished individual who has seamlessly transitioned from being a proficient Application Support Analyst to a Front-End Developer. With a fervent passion for crafting detailed, efficient, and visually appealing projects, this professional brings a unique blend of technical expertise and creative flair to every endeavor. Beyond the world of coding and development, their love for traveling and exploring new places reflects a well-rounded personality with a curiosity for the world. This individual not only excels in the dynamic realm of technology but also embraces the joy of discovering diverse cultures and landscapes. With a commitment to excellence and a zest for exploration, they embody the spirit of a modern, multifaceted professional.
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,32 @@
+
+
+export default function HeroSection() {
+    return (
+      <section id="heroSection" className="hero--section">
+        <div className="hero--section--content--box">
+          <div className="hero--section--content">
+            <p className="section--title">Hey there👋, I'm Michael!</p>
+            <h1 className="hero--section--title">
+              <span className="hero--section--title--color">Front End
+              <br />
+              <span className="hero--section--title--color--dev">Developer </span>
+              </span>
+            </h1>
+            <p className="hero--section-description">
+            I am deeply passionate about front-end designing, where creativity converges with functionality to create seamless and visually appealing user experiences. My proficiency as an application support analyst has equipped me with a robust set of skills that seamlessly transfer to front-end design.
+            </p>
+          </div>
+          
+          <br /><br />
+          <a target="_blank" rel="noopener noreferrer" href='./img/resume/Michael-Gayle-Resume.pdf'>
+          <button  className="btn btn-primary">Download CV</button>
+          </a>
+          
+        </div>
+        <div className="hero--section--img"> 
+          <img src="./img/hero.png" alt="Hero Section" />
+        </div>
+      </section>
+    );
+  }
+  
